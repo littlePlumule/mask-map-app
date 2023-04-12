663829ee59d069b31208023faa3b9b7a16edb154
hung <ff789456741@gmail.com>
2023-04-12 15:34:32 +0800

    add: component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <script setup>
+import LightBox from './components/LightBox.vue';
+import AsideMenu from './components/AsideMenu.vue';
+import MaskMap from './components/MaskMap.vue';
+import { mapActions } from '@/utils/mapStore';
+import { onMounted } from 'vue';
+
+const { fetchLocations, fetchPharmacies } = mapActions();
+
+onMounted(() => {
+  fetchLocations();
+  fetchPharmacies();
+})
 </script>
 
 <template>
-  <div></div>
+  <div id="app">
+    <AsideMenu/>
+    <MaskMap/>
+    <LightBox />
+  </div>
 </template>
 
-<style>
-</style>
+<style lang="scss" src="@/assets/scss/style.scss"></style>
